6893bfec62cba2d38741820f7e7dbe9b3d13e86f
Zack Michener <zack@zackmichener.com>
2012-12-25 01:48:34 -0500

    add instructions

--- ms.py
+++ ms.py
@@ -3,6 +3,16 @@
 from game import Game
 
 if __name__ == "__main__":
-    print("Hello!")
+    print('''Minesweeper!
+
+This is a clone of minesweeper.  To win, mark all the mines with a flag.
+Select a tile by entering the row and column number, separated by a single
+space, like this:
+
+> 4 7
+
+Currently there is no mechanism to flag a mine.
+To quit, type "quit"
+''')
     g = Game()
     g.play()
