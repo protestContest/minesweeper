13d23dfb1e2ceea23ccd20212765bb9612d07b36
Zack Michener <zack@zackmichener.com>
2012-12-24 23:46:59 -0500

    add print to cell

--- cell.py
+++ cell.py
@@ -1,6 +1,11 @@
 class Cell:
-
-    def __init__(self):
-        self.isMine = False
+    def __init__(self, isMine = False):
+        self.isMine = isMine
         self.isVisible = False
         self.value = 0
+
+    def print(self):
+        if self.isMine:
+            print("*", end='')
+        else:
+            print(self.value, end='')
