94fb96b11238aaa2e775e41a08622a3449100213
Zack Michener <zack@zackmichener.com>
2012-12-25 00:13:48 -0500

    assign values to cells

--- board.py
+++ board.py
@@ -16,6 +16,17 @@ class Board:
             self.grid[i] = [0] * y
             for j in range(y):
                 self.grid[i][j] = Cell(random.choice([True, False]))
+        
+        for i in range(x):
+            for j in range(y):
+                curVal = 0
+                for di in range(i-1, i+1):
+                    for dj in range(j-1, j+1):
+                        if di < 0 or di >= x or dj < 0 or dj > y:
+                            continue
+                        if self.grid[di][dj].isMine:
+                            curVal += 1
+                self.grid[i][j].value = curVal
 
     def print(self, showAll=False):
         for i in range(self.rows):
